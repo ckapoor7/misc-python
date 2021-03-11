1480e2455cb1e74f10015f1b06eb8f06f5ba20f5
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-03-11 14:33:38 +0530

    added zebra_puzzle function

--- zebra-puzzle.py
+++ zebra-puzzle.py
@@ -1,7 +1,51 @@
 import itertools
+import time
 
-houses = [1, 2, 3, 4, 5]
-orderings = list(itertools.permutations(houses))
+
+def zebra_puzzle():
+  """
+  There are five houses.
+The Englishman lives in the red house.
+The Spaniard owns the dog.
+Coffee is drunk in the green house.
+The Ukrainian drinks tea.
+The green house is immediately to the right of the ivory house.
+The Old Gold smoker owns snails.
+Kools are smoked in the yellow house.
+Milk is drunk in the middle house.
+The Norwegian lives in the first house.
+The man who smokes Chesterfields lives in the house next to the man with the fox.
+Kools are smoked in the house next to the house where the horse is kept.
+The Lucky Strike smoker drinks orange juice.
+The Japanese smokes Parliaments.
+The Norwegian lives next to the blue house.
+"""
+
+  houses = first, _, middle, _,_ =[1 ,2 ,3 ,4 ,5]
+  orderings = list(itertools.permutations(houses))
+  g = ((ZEBRA, WATER)
+    for red, green, ivory, yellow, blue in c(orderings)
+    for Englishman, Spaniard, Ukrainian, Norwegian, Japanese in c(orderings)
+    for dog, snails, fox, horse, ZEBRA in c(orderings)
+    for tea, coffee, milk,orange,WATER in c(orderings)
+    for OldGoldSmoker, Kools, Chesterfields,LuckyStrike,parliaments in c(orderings)
+    if red is Englishman
+    if dog is Spaniard
+    if coffee is green
+    if Ukrainian is tea
+    if is_right_of(green, ivory)
+    if OldGoldSmoker is snails
+    if Kools is yellow
+    if milk is middle
+    if Norwegian is first
+    if is_next_to(Chesterfields, fox)
+    if is_next_to(Kools, horse)
+    if LuckyStrike is orange
+    if Japanese is parliaments
+    if Norwegian is blue)
+
+  return next(g)
+
 
 def imright(h1, h2):
     "House h1 is immediately right of h2 if h1-h2 == 1."
@@ -10,3 +54,10 @@ def imright(h1, h2):
 def nextto(h1, h2):
     "Two houses are next to each other if they differ by 1."
     return abs(h1-h2) == 1
+
+def timedcall(fn, *args):
+    "Call function with args; return the time in seconds and result."
+    t0 = time.clock()
+    result = fn(*args)
+    t1 = time.clock()
+    return t1-t0, results
