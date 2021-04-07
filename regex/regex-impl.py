cf52647552ff818d5cd0f21a0dea407be426c44f
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-04-07 20:28:30 +0530

    improve the match function using self defined functions

--- regex/regex-impl.py
+++ regex/regex-impl.py
@@ -14,23 +14,11 @@ def search(pattern, text):
         return match('.*' + pattern ,text) #Match any character any number of times
 
 def match(pattern, text):
-    """Return true if a pattern appears at the start of the text"""
-    if pattern == '':
-        return True #Always occurs
-    elif pattern == '$':
-        return (text == '')
-    elif len(pattern) > 1 and pattern[1] in '*?':
-        p, op, pat = pattern[0], pattern[1], pattern[1:]
-        if op == '*':
-            return match_star(p, pat, text)
-        elif op == '?':
-            if match1(p, text) and match(pat, text[1:]):
-                return True
-            else:
-                return match(pat, text)
-    else:
-        return (match1(pattern[0], text) and
-                match(pattern[1:], text[1:]))
+    """Match patter against the start of the text and returns longest match else None"""
+    remainders = matchset(pattern, text)
+    if remainders:
+        shortest = min(remainders, key=len)
+        return text[:len(text)-len(shortest)]
 
 def match1(p, text):
     """Return true if first character of text matches
@@ -81,6 +69,7 @@ def matchset(pattern, text):
         raise ValueError('unknown pattern: %s' % pattern)
         
 null = frozenset()
+
 
 def components(pattern):
     "Return the op, x, and y arguments; x and y are None if missing."
