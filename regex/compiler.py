90d559b043050a849620eed66b68dba568d00a30
ckapoor7 <chaitanya.7.kapoor@gmail.com>
2021-04-08 14:32:18 +0530

    a slightly different implementation of the compiler

--- regex/compiler.py
+++ regex/compiler.py
@@ -7,18 +7,42 @@ def matchset(pattern, text):
     elif 'alt' == op:
         return matchset(x, text) | matchset(y, text)
 
+def match(pattern, text):
+    "Match pattern against start of text; return longest match found or None."
+    remainders = pattern(text)
+    if remainders:
+        shortest = min(remainders, key=len)
+        return text[:len(text)-len(shortest)]
+
+    
 
 def lit(s): return lambda text: set([text[len(s):]]) if text.startswith(s) else null
 
 def seq(x, y): return lambda text: set().union(*map(y, x(text)))
 
 def alt(x, y): return lambda text: x(text) | y(text)
+
+def oneof(chars): return lambda t: set([t[1:]]) if (t and t[0] in chars) else null
+
+dot = lambda t: set([t[1:]]) if t else null
+
+eol = lambda t: set(['']) if t == '' else null
+
+def star(x): return lambda t: (set([t]) | 
+                               set(t2 for t1 in x(t) if t1 != t
+                                   for t2 in star(x)(t1)))
+
         
 null = frozenset([])
 
 def test():
     g = alt(lit('a'), lit('b'))
     assert g('abc') == set(['bc'])
+    assert match(star(lit('a')), 'aaaaabbbaa') == 'aaaaa'
+    assert match(lit('hello'), 'hello how are you?') == 'hello'
+    assert match(lit('x'), 'hello how are you?') == None
+    assert match(oneof('xyz'), 'x**2 + y**2 = r**2') == 'x'
+    assert match(oneof('xyz'), '   x is here!') == None
     return 'test passes'
 
 print (test())
